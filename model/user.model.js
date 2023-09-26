2189d2762ed11a796c0964317460fde00d8ad6f5
ujjwalparsai <ujjwalparsai109@gmail.com>
2023-09-26 18:12:39 +0530

    backend updatae

--- model/user.model.js
+++ model/user.model.js
@@ -15,7 +15,7 @@ const NewUserSchema = mongoose.Schema(
         },  
         password: {
             type: String,
-            require:true,
+            require:true,   
         },
     }
 )
--- router/chat.route.js
+++ router/chat.route.js
@@ -1,10 +1,10 @@
 import express, { Router } from 'express';
-import { CurrentUser, fatchMassage, reciveMassage, sendMassage } from '../controller/user.controller.js';
+import { CurrentUser, fatchMassage,  sendMassage } from '../controller/user.controller.js';
 
 const route = express.Router();
 
 route.post("/send", sendMassage);
-route.post("/recive", reciveMassage);
+// route.post("/recive", reciveMassage);
 route.post("/count",CurrentUser);
 route.get('/getMessages',fatchMassage);
 export default route;
